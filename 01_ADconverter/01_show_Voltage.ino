892d659bb5e32d0cb5a26603bf7b8b8f9eb675a4
Daizo Sasaki <1100594+dzonesasaki@users.noreply.github.com>
2020-09-24 15:14:48 +0900

    Update 01_show_Voltage.ino

--- 01_ADconverter/01_show_Voltage.ino
+++ 01_ADconverter/01_show_Voltage.ino
@@ -1,21 +1,12 @@
+float gfAveragedValue;
+
 void setup() {
   Serial.begin(115200);
+  gfAveragedValue = 0;
 }
 
-const float FACT_VOLTAGE = 3.3/(float)4096;
-const float WEIGHT_AVERAGE = 1/(float)10;
-const uint32_t NLOOP = 1000;
-float gfAveragedValue =0;
-
 void loop() {
-  float currentValue = 0;
-
-  for(int i=0;i<NLOOP;i++)
-  {
-    currentValue = analogRead(33)*FACT_VOLTAGE;
-    gfAveragedValue = currentValue * WEIGHT_AVERAGE + gfAveragedValue * (1-WEIGHT_AVERAGE); 
-    delay(1);
-  }  
+  gfAveragedValue = analogRead(33) / (float)4096 * 3.3  * 10 * 0.1 + gfAveragedValue * (1-0.1); 
   Serial.println(gfAveragedValue);
-  delay(1);
+  delay(10);
 }
